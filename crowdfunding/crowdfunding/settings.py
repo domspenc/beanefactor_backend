3a2248ab6da6fcbe78b4171f0657397093cb8c6e
domspenc <dommspencer@gmail.com>
2024-12-16 17:31:02 +0800

    added debugging = true to settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -33,9 +33,10 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(
-    'DJANGO_DEBUG'
-) != 'False'
+DEBUG = True
+# DEBUG = os.environ.get(
+#     'DJANGO_DEBUG'
+# ) != 'False'
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
@@ -104,7 +105,7 @@ WSGI_APPLICATION = 'crowdfunding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.BASE_DIR / 'db.sqlite3',
     }
 }
 # db_from_env = dj_database_url.config(conn_max_age=500)
