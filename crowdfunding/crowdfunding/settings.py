f0c19259418bbb8f47c7d75b09ed2ee457223eca
domspenc <dommspencer@gmail.com>
2024-12-16 18:18:50 +0800

    removing os from database in settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'crowdfunding.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # db_from_env = dj_database_url.config(conn_max_age=500)
